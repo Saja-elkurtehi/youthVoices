2547f06578b459c91659c1d30e66f2c7fc1310eb
Saja Elkurtehi <114425874+Saja-elkurtehi@users.noreply.github.com>
2024-07-20 22:53:31 -0400

    -

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/HomePage/HomePage';
 import StoriesPage from './pages/StoriesPage/StoriesPage';
